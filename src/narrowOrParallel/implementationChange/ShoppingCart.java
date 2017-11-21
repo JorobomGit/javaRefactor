11d650b24aeaeb82d046206ac4c094c2f11e09f6
jose <jorollu@hotmail.com>
2017-11-21 10:55:50 +0100

    ShoppingCart refactored from single int to list

--- src/narrowOrParallel/implementationChange/ShoppingCart.java
+++ src/narrowOrParallel/implementationChange/ShoppingCart.java
@@ -1,21 +1,25 @@
 package narrowOrParallel.implementationChange;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart {
-    private int price;
+
+    private List<Integer> prices = new ArrayList<>();
 
     public void add(int price) {
-        this.price = price;
+        this.prices.add(price);
     }
 
     public int calculateTotalPrice() {
-        return price;
+        return prices.stream().mapToInt(Integer::intValue).sum();
     }
 
     public boolean hasDiscount() {
-        return price >= 100;
+        return this.calculateTotalPrice() >= 100;
     }
 
     public int numberOfProducts() {
-        return 1;
+        return this.prices.size();
     }
 }
