076f31ed883755b36c66fca0d83976570584ce01
jose <jorollu@hotmail.com>
2017-11-21 12:32:56 +0100

    Refactor ShoppingCart Integer to Price with refactor inline

--- src/narrowOrParallel/implementationChange/ShoppingCart.java
+++ src/narrowOrParallel/implementationChange/ShoppingCart.java
@@ -5,18 +5,18 @@ import java.util.List;
 
 public class ShoppingCart {
 
-    private List<Integer> prices = new ArrayList<>();
+    private List<Price> prices = new ArrayList<>();
 
-    public void add(int price) {
+    public void add(Price price) {
         this.prices.add(price);
     }
 
-    public int calculateTotalPrice() {
-        return prices.stream().mapToInt(Integer::intValue).sum();
+    public Price calculateTotalPrice() {
+        return new Price(prices.stream().mapToInt(Price::getPrice).sum());
     }
 
     public boolean hasDiscount() {
-        return this.calculateTotalPrice() >= 100;
+        return this.calculateTotalPrice().getPrice() >= 100;
     }
 
     public int numberOfProducts() {
